23040a7f6250acaf2633d6474f589cce87bb343d
aroseca15 <69179642+aroseca15@users.noreply.github.com>
2021-01-11 11:59:50 -0700

    Updated

---
+++ am-portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, this is the start of my professional portfolio.</h1>
+    </div>
+  );
+}
+
+export default App;
