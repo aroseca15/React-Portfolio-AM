bb9b8ae92e74e4939772bcbc027d7184276b8a01
Your Name <aroseca15@gmail.com>
2021-02-09 16:52:38 -0700

    worked with home.js CSS

--- am-portfolio/src/pages/Home.js
+++ am-portfolio/src/pages/Home.js
@@ -6,22 +6,23 @@ const SanFran = require('../assets/images/Aquarium.jpg')
 function Home() {
     return (
         <main className='container-fluid home'>
-            <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+            <section  className='row'>
+                <div>
+                    <h1></h1>
+                </div>
             </section>
 
-            <section className='row justify-content-center'>
+            <section id='row1' className='row justify-content-center'>
                 <h1 id='introHome' className="display-4">Welcome!! I am Amanda and I am a Full-Stack web developer.</h1>
             </section>
 
             <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+                <h1></h1>
             </section>
 
             <section className='row'>
-                <div className='card'>
-                    <image src='https://www.clker.com/cliparts/D/U/c/C/P/i/team-work-hi.png'></image>
-                    <div className='card-bodi'>
+                <div id= 'homeCard' className='card'>
+                    <div id= 'homeCard' className='card-bodi'>
                         <p className='card-text'>I have a passion for learning new things and problem solving. Coding is a perfect blend of those two passions. Although I am capable of thriving on an individual basis, I feel the best ideas and work are from the efforts of a team.</p>
                         <a href="/portfolio" id='jumpTo' class="btn btn-info">Jump Right To My Work</a>
                     </div>
@@ -29,23 +30,23 @@ function Home() {
             </section>
 
             <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+                <h1></h1>
             </section>
 
             <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+                <h1></h1>
             </section>
 
             <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+                <h1></h1>
             </section>
 
             <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+                <h1></h1>
             </section>
 
             <section className='row'>
-                <h1>Hi, this is the start of my professional portfolio.</h1>
+                <h1></h1>
             </section>
         </main>
     )
